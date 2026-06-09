b81c9e61ceb66cec2b559e7c09f231a32c07760e
babuatul1234 <atulbabu5636@gmail.com>
2026-06-09 12:12:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,19 +21,6 @@ features = list(model.feature_names_in_)
 gender = st.selectbox("Gender", ["Male", "Female"])
 age = st.number_input("Age", min_value=1, max_value=120, value=30)
 
-smoking = st.selectbox("Smoking", [1, 2])
-yellow_fingers = st.selectbox("Yellow Fingers", [1, 2])
-anxiety = st.selectbox("Anxiety", [1, 2])
-peer_pressure = st.selectbox("Peer Pressure", [1, 2])
-chronic_disease = st.selectbox("Chronic Disease", [1, 2])
-fatigue = st.selectbox("Fatigue", [1, 2])
-allergy = st.selectbox("Allergy", [1, 2])
-wheezing = st.selectbox("Wheezing", [1, 2])
-alcohol_consuming = st.selectbox("Alcohol Consuming", [1, 2])
-coughing = st.selectbox("Coughing", [1, 2])
-shortness_of_breath = st.selectbox("Shortness of Breath", [1, 2])
-swallowing_difficulty = st.selectbox("Swallowing Difficulty", [1, 2])
-chest_pain = st.selectbox("Chest Pain", [1, 2])
 
 def yes_no(label):
     return 2 if st.selectbox(label, ["No", "Yes"]) == "Yes" else 1
